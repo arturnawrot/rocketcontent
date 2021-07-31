aded84bcb3573b8f589c883b31aaa565ef8f8879
arturnawrot <artur.programista2@gmail.com>
2021-07-31 04:54:18 -0400

    Showing phpinfo only in local environment

--- app/Http/Controllers/Dev/ShowPHPInfoController.php
+++ app/Http/Controllers/Dev/ShowPHPInfoController.php
@@ -3,11 +3,16 @@
 namespace App\Http\Controllers\Dev;
 
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\App;
 
 class ShowPHPInfoController extends Controller
 {
     public function show()
     {
+        if(!App::environment(['local'])) {
+            return abort(403);
+        }
+
         return phpinfo();
     }
 }
