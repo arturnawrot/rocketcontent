7f2709404bcbba7911dd8d2e61a61c5464b91ac8
arturnawrot <artur.programista2@gmail.com>
2021-08-08 00:34:00 -0400

    Free trial message added

--- resources/views/landing-page-sections/hero-section.blade.php
+++ resources/views/landing-page-sections/hero-section.blade.php
@@ -4,18 +4,18 @@
             <div class="col-12 col-md-4 col-lg-5 order-md-2 d-flex justify-content-end">
                 <img src="/svg/rocket-hero.svg" id="hero-image" class="img-fluid mw-md-150 mw-lg-130 mb-6 mb-md-0" alt="...">
             </div>
-            
+
             <div class="col order-md-1" data-aos="fade-up">
 
             <h1 class="display-3 text-center text-md-start" id="heading">
                 Lorem <span id="the-highlighted-word">ipsum</span> dolor sit amet. Consectetur adipiscing
             </h1>
 
-            <p class="lead text-center text-md-start text-muted mb-6 mb-lg-8 mt-5" id="hero-description">
+            <p class="lead text-center text-md-start text-muted mb-6 mb-lg-8 mt-4" id="hero-description">
                 Donec volutpat justo eu lectus tristique porta. Praesent laoreet vehicula magna.
             </p>
 
-            <div class="pt-5">
+            <div class="pt-4">
                 <div class="text-center text-md-start">
                     <a href="docs/index.html" class="button" id="free-trial-button">
                         Start Your Free Trial
@@ -23,6 +23,9 @@
                 </div>
             </div>
 
+            <div class="pt-5" id="free-trial-message">
+                14-Day Free trial for new customers.
+            </div>
 
             </div>
         </div>
