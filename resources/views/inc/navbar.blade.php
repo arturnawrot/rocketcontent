ef119e8627c7e1099d93768e5cce6fe1267c9f89
arturnawrot <artur.programista2@gmail.com>
2021-08-08 00:26:17 -0400

    Landing page - navbar and the hero section

---
+++ resources/views/inc/navbar.blade.php
@@ -0,0 +1,30 @@
+<nav class="navbar navbar-expand-lg">
+  <div class="container navbar-container">
+    <a class="navbar-brand" href="#">rocketcontent.io</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav mx-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link" href="#">About</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Product</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Pricing</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Resources</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Jobs</a>
+        </li>
+      </ul>
+      <ul class="navbar-nav d-none d-lg-flex ml-2 order-3">
+          <li class="nav-item"><a class="nav-link button" href="https://themes.getbootstrap.com/signin/">Client Area</a></li>
+      </ul>
+    </div>
+  </div>
+</nav>
---
+++ resources/views/landing-page-sections/hero-section.blade.php
@@ -0,0 +1,30 @@
+<section class="pt-4 pt-md-11 hero-section">
+    <div class="container">
+        <div class="row align-items-center">
+            <div class="col-12 col-md-4 col-lg-5 order-md-2 d-flex justify-content-end">
+                <img src="/svg/rocket-hero.svg" id="hero-image" class="img-fluid mw-md-150 mw-lg-130 mb-6 mb-md-0" alt="...">
+            </div>
+            
+            <div class="col order-md-1" data-aos="fade-up">
+
+            <h1 class="display-3 text-center text-md-start" id="heading">
+                Lorem <span id="the-highlighted-word">ipsum</span> dolor sit amet. Consectetur adipiscing
+            </h1>
+
+            <p class="lead text-center text-md-start text-muted mb-6 mb-lg-8 mt-5" id="hero-description">
+                Donec volutpat justo eu lectus tristique porta. Praesent laoreet vehicula magna.
+            </p>
+
+            <div class="pt-5">
+                <div class="text-center text-md-start">
+                    <a href="docs/index.html" class="button" id="free-trial-button">
+                        Start Your Free Trial
+                    </a>
+                </div>
+            </div>
+
+
+            </div>
+        </div>
+    </div>
+</section>
